4c3a7a31029c0d0f5e5a24d63b37aaf353507769
it-AVNG <it.avng@gmail.com>
2022-12-16 23:42:24 +0700

    Add sliders feature to update the grid container
    Add reset button to clean the grid container after drawn

--- javascript.js
+++ javascript.js
@@ -1,44 +1,60 @@
-//function to update gridTemplateColumns
-function updateTemplate(){
+//access a variable grid container
+var gridContainer = document.querySelector("#gridContainer");
+
+//function to update gridTemplateColumns inline styling
+function updateTemplate(sideNumber){
   let string = '';
-  string =`repeat(${divSide}, 1fr)`;
+  string =`repeat(${sideNumber}, 1fr)`;
   return string;
 }
-
-//create function to update the sketchpad
-function updateSketchPad(){
-  
+//create function to update grid container
+function updateGridContainer(sideNumber){
   //create a loop to append the child div
-  for(let i=1; i<=divSize; i++){
+  for(let i=1; i<=gridSize; i++){
     //create a grid element div
     const childDiv = document.createElement("div");
     // set the child class name
     childDiv.className = 'grid-items';
-    //set the child to listen to event and change the color
-    childDiv.addEventListener("mousedown",function(){
-      childDiv.style.backgroundColor = 'white';
-    });
-   
     //append the element to the container
     gridContainer.appendChild(childDiv);
-
   };
 
   //create a method to access and update grid template columns accordingly
   document.getElementById(
     "gridContainer"
-  ).style.gridTemplateColumns = updateTemplate(divSide);
+  ).style.gridTemplateColumns = updateTemplate(sideNumber);
 
 };
+//a Function to clear all child node
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+      parent.removeChild(parent.firstChild);
+  }
+}
 
 
-//access a variable grid container
-const gridContainer = document.querySelector("#gridContainer");
-//create a variable holds the size of the divs
-const divSide = 16; //16x16 divs
-const divSize = Math.pow(divSide, 2);
-//update the sketchpad with the above parameters
-updateSketchPad();
+
+
+
+//Main program:
+
+//add sliders presentation method
+var slider = document.getElementById("gridSide");
+var sliderOutput = document.getElementById("sliderVal");
+//update slider value
+sliderOutput.innerHTML = slider.value;
+slider.oninput = function() {
+  sliderOutput.innerHTML = this.value;
+}
+
+
+//check grid size from previous build
+var gridSide =slider.value; 
+var gridSize = Math.pow(gridSide, 2);
+//update the grid container with the above parameters
+removeAllChildNodes(gridContainer);
+updateGridContainer(gridSide);
+
 
 //delegate the event listener to the container for mouseover event
 gridContainer.addEventListener(
@@ -47,10 +63,25 @@ gridContainer.addEventListener(
     if (target.className === 'grid-items'){ 
       e.target.style.backgroundColor= "black";
     };
-   
   }
 );
 
+
+//listen to slider changes
+slider.addEventListener('input', function(){
+  //update the grid container with the change
+
+  //remove the previous node
+  removeAllChildNodes(gridContainer);
+  //update the new node
+  gridSide =slider.value;
+  gridSize = Math.pow(gridSide, 2);
+  updateGridContainer(gridSide);
+
+})
+
+
+//end Main program
 
 
 
